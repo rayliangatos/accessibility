c912c073c7ccb6fb82e49dba882e67a5cc2342f0
rayliangatos <57181888+rayliangatos@users.noreply.github.com>
2020-05-19 17:59:56 +0100

    associate dropdown from lines to stops (#2)

--- searchbus.php
+++ searchbus.php
@@ -5,11 +5,12 @@
 <link rel="stylesheet" href="fontawesome/css/all.css" rel="stylesheet">
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php
   require 'vendor/autoload.php';
   use Nahid\JsonQ\Jsonq;
 
-  $json=$busLine=$busStop=$leaveTime=$nextTime= "";
+  $jsonData=$busLine=$busStop=$leaveTime=$nextTime= "";
 
   if($_SERVER['REQUEST_METHOD'] == "POST") {
       searchResult();
@@ -20,8 +21,8 @@
     $busStop = isset($_POST['busStop']) ? $_POST['busStop'] : '';
     $leaveTIme = isset($_POST['leaveTime']) ? $_POST['leaveTime'] : '';
 
-    $q = new Jsonq('bustimetable.json');
-    $line = $q->from("lines")
+    $jsonData = new Jsonq('bustimetable.json');
+    $line = $jsonData->from("lines")
     ->where('id', '=', $busLine)
     ->get();
 
@@ -54,30 +55,120 @@
   }
 
 ?>
+<script type="text/javascript">
+  function changeStops(){
+    var selectedBusLine = $('#busLine').children("option:selected").val();
+    if (selectedBusLine=='8') {
+      $('option[class^="bus9"]').hide();
+      $('option[class^="bus8"]').show();
+    } else if(selectedBusLine=='9'){
+      $('option[class^="bus8"]').hide();
+      $('option[class^="bus9"]').show();
+    }
+  }
+</script>
 </head>
-  <body>
+  <body onload="changeStops()">
     <div class="container">
       <h2>Search Bus</h2>
       <form class="" action="searchbus.php" method="post">
         <div class="form-group row">
           <label for="busLine" class="col-sm-3 col-form-label">Your Bus Line:</label>
           <div class="col-sm-9">
-            <select class="custom-select" id="busLine" name="busLine">
-              <option value="8">8</option>
-              <option value="9">9</option>
+            <select class="custom-select" id="busLine" name="busLine" onchange="changeStops()">
+              <option value="8"
+              <?php if(isset($_POST['busLine']) && $_POST['busLine'] == '8')
+              echo ' selected="selected"';
+              ?>
+              >8</option>
+              <option value="9"
+              <?php if(isset($_POST['busLine']) && $_POST['busLine'] == '9')
+              echo ' selected="selected"';
+              ?>
+              >9</option>
             </select>
           </div>
         </div>
         <div class="form-group row">
           <label for="busStop" class="col-sm-3 col-form-label">Your current Stop:</label>
           <div class="col-sm-9">
-            <select class="custom-select" id="busStop" name="busStop" >
-              <option value="St Mary's Butts">St Mary's Butts</option>
-              <option value="Station Road">Station Road</option>
-              <option value="RBH South Wing">RBH South Wing</option>
-              <option value="TVSP">TVSP</option>
-              <option value="School Green">School Green</option>
-              <option value="Bays Crescent">Bays Crescent</option>
+            <select class="custom-select" id="busStop" name="busStop">
+              <option value="">--</option>
+              <option value="St Mary's Butts" class="bus8"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'St Mary\'s Butts')
+              echo ' selected="selected"';
+              ?>
+              >St Mary's Butts</option>
+              <option value="Station Road" class="bus8"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'Station Road')
+              echo ' selected="selected"';
+              ?>
+              >Station Road</option>
+              <option value="RBH South Wing" class="bus8"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'RBH South Wing')
+              echo ' selected="selected"';
+              ?>
+              >RBH South Wing</option>
+              <option value="TVSP" class="bus8"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'TVSP')
+              echo ' selected="selected"';
+              ?>
+              >TVSP</option>
+              <option value="School Green" class="bus8"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'School Green')
+              echo ' selected="selected"';
+              ?>
+              >School Green</option>
+              <option value="Bays Crescent" class="bus8"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'Bays Crescent')
+              echo ' selected="selected"';
+              ?>
+              >Bays Crescent</option>
+              <option value="St Mary's Butts" class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'St Mary\'s Butts')
+              echo ' selected="selected"';
+              ?>
+              >St Mary's Butts</option>
+              <option value="Station Road" class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'Station Road')
+              echo ' selected="selected"';
+              ?>
+              >Station Road</option>
+              <option value="RBH South Wing" class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'RBH South Wing')
+              echo ' selected="selected"';
+              ?>
+              >RBH South Wing</option>
+              <option value="Community Centre" class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'Community Centre')
+              echo ' selected="selected"';
+              ?>
+              >Community Centre</option>
+              <option value="Hartland Road"  class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'Hartland Road')
+              echo ' selected="selected"';
+              ?>
+              >Hartland Road</option>
+              <option value="Wentworth Avenue"  class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'Wentworth Avenue')
+              echo ' selected="selected"';
+              ?>
+              >Wentworth Avenue</option>
+              <option value="Three Mile Cross Post Office"  class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'Three Mile Cross Post Office')
+              echo ' selected="selected"';
+              ?>
+              >Three Mile Cross Post Office</option>
+              <option value="St Michaels Hall"  class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'St Michaels Hall')
+              echo ' selected="selected"';
+              ?>
+              >St Michaels Hall</option>
+              <option value="Bays Crescent"  class="bus9"
+              <?php if(isset($_POST['busStop']) && $_POST['busStop'] == 'Bays Crescent')
+              echo ' selected="selected"';
+              ?>
+              >Bays Crescent</option>
             </select>
           </div>
         </div>
